317ca0c2474dfcce2c850672a3851108f3ac1bc1
Rodrigo Rafael <rodrigo_medy@hotmail.com>
2019-06-26 09:58:01 -0300

    adicionado buscador de gifs

---
+++ buscador_gif/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gif/ui/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
