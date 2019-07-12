10215c367def401c3f027bd9674c1b54cee8c9aa
Rodrigo Rafael <rodrigo_medy@hotmail.com>
2019-07-12 10:58:09 -0300

    configurado Firebase e AndroidX

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  return runApp(MyApp());
+  Firestore.instance
+      .collection("teste")
+      .document('teste')
+      .setData({'teste': 'teste'});
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
